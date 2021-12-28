498061fa7be37d2694af784f6c60029ec93311dd
Devin Dixon <development@bingewave.com>
2021-12-28 08:53:55 -0500

    Fixes for complete example

--- widget.js
+++ widget.js
@@ -1,21 +1,23 @@
+
 let youTubeRegExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
 
 function displayVideo() {
-  
-  	let url = document.getElementById(BWProperties.namespace + '_youtube_url').value;
-  
+	
+	let url = document.getElementById(BWProperties.namespace + '_youtube_url').value;
+
   	let valid =  isValidYoutubeVideo(url);
   
-    if(valid) {
+    	if(valid) {
       		let iframe= createEmbedElement(url);
       
       		let content = `
             	<div class="text-center">
                 		${iframe}
+                        <button class="btn btn-danger" onclick="{{namespace}}.closeLocalVideo('{{overlay_id}}')">Close</button>
                 </div>
             `;
       
-      		let params = {content : content, wrap_content : false};
+      		let params = {content : content, wrap_content : false, timeout: -1, execute_javascript : '_3rdVz.flushOverlayToTop("{{overlay_id}}")'};
     
      		BWAPI.post('/events/' + BWProperties['event_id'] + '/sendOnscreenContent', params);
       
@@ -29,24 +31,45 @@ function isValidYoutubeVideo(url) {
   
 	if(!url) {
  		return false; 
-    }
-  
-    let match = url.match(youTubeRegExp);
+    	}
+  	
+    	let match = url.match(youTubeRegExp);
 	
   	if (match && match[2].length == 11) {
           return true;
-    }
+    	}
      
   	return false;
 }
 
 function createEmbedElement(url){
-  
-  	let match = url.match(youTubeRegExp);
+	
+    	var match = url.match(youTubeRegExp);
   
   	let iframe = '<iframe src="https://www.youtube.com/embed/' + match[2] + '?autoplay=1" height="{{screen_height}}" width="{{screen_width}}"></iframe> ';
   
   	return iframe;
 }
 
+function closeLocalVideo(overlay_id){
+  
+  	let element = document.getElementById(overlay_id);
+  
+ 	if(element){
+  	 	element.parentNode.removeChild(element);
+    	}
+}
+
+function flushOverlayToTop(overlay_id) {
+  
+	let element = document.getElementById(overlay_id);
+  
+ 	if(element){
+  	 	element.style="top: 0px;";
+    	}
+  
+}
+
 this.displayVideo = displayVideo;
+this.closeLocalVideo = closeLocalVideo;
+this.flushOverlayToTop = flushOverlayToTop;
